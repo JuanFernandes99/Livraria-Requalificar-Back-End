ad379972158e87755fa49800e5c3a54fb29e5d7f
jbarrinhos <96847584+jbarrinhos@users.noreply.github.com>
2022-03-18 22:46:03 +0000

    cliente.uptade

--- src/main/java/livrariaRq/controller/ClienteController.java
+++ src/main/java/livrariaRq/controller/ClienteController.java
@@ -1,8 +1,11 @@
 package livrariaRq.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +54,11 @@ public class ClienteController {
 			src.setMessage("Ocorreu um erro");
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(src);
 		}
+	}
+
+	@GetMapping("/getClientes")
+	public List<Cliente> getClientes() {
+		return clienteService.getClientes();
 	}
 
 }
