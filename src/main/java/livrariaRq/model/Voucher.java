ab6a8ade728b104f8c408fd3a29cf3a9eb9b94f5
JuanFernandes99 <juandanieldosr@gmail.com>
2022-03-22 21:38:26 +0000

    Update Voucher.java

--- src/main/java/livrariaRq/model/Voucher.java
+++ src/main/java/livrariaRq/model/Voucher.java
@@ -1,9 +1,21 @@
 package livrariaRq.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Voucher")
 public class Voucher {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
 	@OneToOne(mappedBy = "voucher")
 	private Compra compra;
 
